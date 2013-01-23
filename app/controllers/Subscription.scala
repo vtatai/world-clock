21218b86062810c7d68003ea8906365d367277a6
Victor Tatai <vtatai@gmail.com>
2013-01-23 10:02:52 -0300

    Testing interaction with appdirect

--- app/controllers/Subscription.scala
+++ app/controllers/Subscription.scala
@@ -13,9 +13,16 @@ import models._
 object Subscription extends Controller {
   val Home = Redirect(routes.Application.index)
 
-  def create() = Action {
-    Logger.info("Create action called")
-    Home
+  def create() = Action { implicit request =>
+    Logger.info("Create action called " + request)
+    val oauthNonce = request.headers("oauth_nonce")
+    val oauthTs = request.headers("oauth_timestamp")
+    val oauthConsumerKey = request.headers("oauth_consumer_key")
+    val oauthSignatureMethod = request.headers("oauth_signature_method")
+    val oauthVersion = request.headers("oauth_version")
+    val oauthSignature = request.headers("oauth_signature")
+    Logger.info("headers: %s | %s | %s | %s | %s | %s" format (oauthNonce, oauthTs, oauthConsumerKey, oauthSignatureMethod, oauthVersion, oauthSignature))
+    Ok
   }
   
   def update() = Action {
